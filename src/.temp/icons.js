c4f4e001095eda2d151c63efebbcbd778d801b8f
Josh <JoshAFairhead@gmail.com>
2020-06-01 17:32:06 +0200

    added site

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=b0cc2fd"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=b0cc2fd"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=b0cc2fd"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=b0cc2fd"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=b0cc2fd"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=6c639fa"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=6c639fa"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=6c639fa"
+    }
+  ]
+}
