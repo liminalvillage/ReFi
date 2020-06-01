125ce5db41e6ebe3da3cae698b6bb17293f483a4
Maija <hello@maijagrudule.com>
2020-06-01 23:06:22 +0200

    Remove .temp files from repo

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Gridsome Portfolio Starter",
-  "siteUrl": "https://gridsome-portfolio-starter.netlify.com",
-  "version": "0.7.14",
-  "catchLinks": true
-}
